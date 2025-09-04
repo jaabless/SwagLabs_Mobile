6291a2a6f63228ce2e7e0dbd36818da5182a2797
jaabless <jefferyasamoahbless@gmail.com>
2025-09-04 16:38:42 +0000

    Add CheckoutInformationPage class for user info input

---
+++ src/main/java/com/swaglabs/pages/CheckoutInformationPage.java
@@ -0,0 +1,47 @@
+package com.swaglabs.pages;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class CheckoutInformationPage extends BasePage {
+
+    @AndroidFindBy(accessibility = "test-First Name")
+    private WebElement firstNameField;
+
+    @AndroidFindBy(accessibility = "test-Last Name")
+    private WebElement lastNameField;
+
+    @AndroidFindBy(accessibility = "test-Zip/Postal Code")
+    private WebElement zipCodeField;
+
+    @AndroidFindBy(accessibility = "test-CONTINUE")
+    private WebElement continueButton;
+
+//    @AndroidFindBy(accessibility = "test-Error message")
+    @AndroidFindBy(xpath = "//android.view.ViewGroup[@content-desc=\"test-Error message\"]")
+    private WebElement errorMessage;
+
+    public CheckoutInformationPage(AppiumDriver driver) {
+        super(driver);
+    }
+
+    public void enterUserInformation(String firstName, String lastName, String zipCode) {
+        sendKeysToElement(firstNameField, firstName);
+        sendKeysToElement(lastNameField, lastName);
+        sendKeysToElement(zipCodeField, zipCode);
+    }
+
+    public void clickContinue() {
+        clickElement(continueButton);
+    }
+
+    public String getErrorMessage() {
+        return errorMessage.getText();
+    }
+
+    public boolean isCheckoutInfoPageDisplayed() {
+        return firstNameField.isDisplayed() && lastNameField.isDisplayed() && zipCodeField.isDisplayed();
+    }
+}
